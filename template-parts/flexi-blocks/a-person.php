bd3927de3f3d132cec8abbe7c2cacdebc3d758f7
alimont01 <hello@alimontgomery.com>
2026-09-03 15:09:10 +0100

    wip

--- template-parts/flexi-blocks/a-person.php
+++ template-parts/flexi-blocks/a-person.php
@@ -1,26 +1,46 @@
-<div class="container mb-4 remove-p-margin">
-	<div class="row">
-		<div class="col-12">
-			<div class="bg-light-grey d-flex flex-wrap">
-				<?php 
-				$image = get_sub_field('person_picture');
-				if( !empty( $image ) ): ?>
-					<div class="col-3">
-						<img class="w-100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-					</div>
-				<?php endif; ?>
-				<div class="col p-4">
-					<?php if( get_sub_field('persons_name') ): ?>
-						<h4 class="mt-0 mb-1 text-blue"><?php the_sub_field('persons_name'); ?></h4>
-					<?php endif; ?>
-					<?php if( get_sub_field('persons_position') ): ?>
-						<p class="fw-bold mb-3"><?php the_sub_field('persons_position'); ?></p>
-					<?php endif; ?>
-					<?php if( get_sub_field('bio') ): ?>
-						<p><?php the_sub_field('bio'); ?></p>
-					<?php endif; ?>
+
+
+<div class="container-fluid pb-4 mb-4 mb-lg-5 bg-light-grey">
+
+	<?php if( get_sub_field('block_title_team') ): ?>
+		<div class="container py-4 py-lg-5">
+			<div class="row">
+				<div class="col-12 text-center">
+					<h2 class="text-blue">
+						<?php the_sub_field('block_title_team'); ?>
+					</h2>
 				</div>
 			</div>
 		</div>
+	<?php endif; ?>
+
+	<?php if( have_rows('add_a_person') ): ?>
+	<div class="container mb-4 remove-p-margin">
+		<div class="row">
+			<?php while( have_rows('add_a_person') ): the_row(); 
+			$image = get_sub_field('person_picture');
+			?>
+				<div class="col-12">
+					<div class="bg-white d-flex flex-wrap">
+						<div class="col-3">
+							<?php echo wp_get_attachment_image( $image, 'full', "", ["class" => "w-100"] ); ?>
+						</div>
+						<div class="col p-4">
+							<?php if( get_sub_field('persons_name') ): ?>
+								<h4 class="mt-0 mb-1 text-blue"><?php echo acf_esc_html( get_sub_field('persons_name') ); ?></h4>
+							<?php endif; ?>
+							<?php if( get_sub_field('persons_position') ): ?>
+								<p class="fw-bold mb-3"><?php echo acf_esc_html( get_sub_field('persons_position') ); ?></p>
+							<?php endif; ?>
+							<?php if( get_sub_field('bio') ): ?>
+								<p><?php echo acf_esc_html( get_sub_field('bio') ); ?></p>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		</div>
 	</div>
+	<?php endif; ?>
+	
 </div>
--- template-parts/flexi-blocks/staff-news-feed.php
+++ template-parts/flexi-blocks/staff-news-feed.php
@@ -1,17 +1,17 @@
+	<div class="container-fluid mb-4 mb-lg-5">
 
-    <div class="container py-4 py-lg-5 text-blue">
-        <div class="row">
-            <div class="col-12 text-center">
-				<h2 class="">
-					Latest Staff News
-				</h2>
-            </div>
-        </div>
-    </div>
+		<div class="container py-4 py-lg-5">
+			<div class="row">
+				<div class="col-12 text-center">
+					<h2 class="text-blue">
+						Latest Staff News
+					</h2>
+				</div>
+			</div>
+		</div>
 
-	<div class="container-fluid pb-4 pb-lg-5">
         <div class="container">
-                <div class="row g-4">
+            <div class="row g-4">
                     <?php $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
                         $query_args = array(
                             'post_type' => 'post',
@@ -28,7 +28,7 @@
                     $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'news-post' );
                     ?>
 
-						<div class="col-sm-3">
+						<div class="col-sm-6 col-lg-4 mb-4">
 							<?php if ( has_post_thumbnail() ) : ?>
 								<img class="w-100" src="<?php echo $thumb['0'];?>" alt="<?php the_title(); ?>">
 							<?php else: ?> 
@@ -39,26 +39,25 @@
 								echo wp_get_attachment_image( $image, $size, "",array('class' => 'w-100') );
 								} ?>
 							<?php endif; ?> 
-						</div>
-						<div class="col-sm-9">
-							<h2 class="mt-0 mb-4 fs-3 text-blue">
+
+							<h2 class="mt-3 mb-4 fs-4 text-blue">
 								<?php the_title(); ?>
 							</h2>
-								<?php
-								$excerpt = get_the_excerpt();
-								$limit   = 45;
+							<?php
+							$excerpt = get_the_excerpt();
+							$limit   = 45;
 
-								echo wp_trim_words( $excerpt, $limit );
+							echo wp_trim_words( $excerpt, $limit );
 
-								if ( str_word_count( wp_strip_all_tags( $excerpt ) ) > $limit ) : ?>
-									
-									<a href="<?php the_permalink(); ?>">Read more</a>
+							if ( str_word_count( wp_strip_all_tags( $excerpt ) ) > $limit ) : ?>
+								
+								<a href="<?php the_permalink(); ?>">Read more</a>
 
-								<?php endif; ?>
+							<?php endif; ?>
 						</div>
 
                     <?php endwhile; ?> 
-                </div>
+            </div>
 
                 <?php else: ?>
                 <article>
@@ -66,16 +65,16 @@
                     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
                 </article>
                 <?php endif; ?>
-            </div>
         </div>
-    </div>
 
-	<div class="container pb-4 pb-lg-5">
-        <div class="row">
-			<div class="col-12 text-center">
-				<a class="btn-blue mt-2 d-inline-block" href="/category/staff-news/">
-					View all Staff News
-				</a>
+		<div class="container mb-3">
+			<div class="row">
+				<div class="col-12 text-center">
+					<a class="btn-blue mt-2 d-inline-block" href="/category/staff-news/">
+						View all Staff News
+					</a>
+				</div>
 			</div>
-        </div>
-    </div>
+		</div>
+
+</div>
