85a53d5eb51e3ead711d65fda8a577669ce6075c
alimont01 <hello@alimontgomery.com>
2026-09-09 12:26:23 +0100

    hp slider

--- front-page.php
+++ front-page.php
@@ -7,8 +7,75 @@ get_header();
 ?>
 
 
-    <?php get_template_part( 'template-parts/banner' ); ?>
+	<div class="container-fluid position-relative d-flex align-items-center mb-4 mb-lg-2 bg-grad text-white hp-hero">
+			<div class="container pt-5 pt-lg-2 pb-4 py-lg-5 z-top-top">
+				<div class="row justify-content-center">
+					<div class="col-lg-8 text-center">
+						<h1 class="mt-0 mb-4">
+							<?php the_title(); ?>
+						</h1>
+						<?php if( get_field('hero_intro_text') ): ?>
+							<p class=""><?php the_field('hero_intro_text'); ?></p>
+						<?php endif; ?>
+
+						<div class="w-100 d-flex justify-content-center flex-wrap">
+							<?php 
+							$link = get_field('hero_button_link');
+							if( $link ): 
+								$link_url = $link['url'];
+								$link_title = $link['title'];
+								$link_target = $link['target'] ? $link['target'] : '_self';
+								?>
+								<a class="btn-white mx-2 mb-3" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+							<?php endif; ?>
+							<?php 
+							$link_secondary = get_field('hero_button_link_secondary');
+							if( $link_secondary ): 
+								$link_url = $link_secondary['url'];
+								$link_title = $link_secondary['title'];
+								$link_target = $link_secondary['target'] ? $link['target'] : '_self';
+								?>
+								<a class="btn-outline mx-2 mb-3" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Swiper -->
+            <?php if( have_rows('carousel_images') ): ?>
+                <div class="swiper hpSwiper position-absolute start-0 top-0">
+                    <div class="swiper-wrapper">
+                        <?php while( have_rows('carousel_images') ): the_row(); 
+                        $image = get_sub_field('add_image_carousel');
+                        ?>
+                            <div class="swiper-slide">
+                                <?php echo wp_get_attachment_image( $image, 'full' ); ?>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+			<div class="position-absolute start-0 left-0 w-100 h-100 z-top bg-grad opacity-75"></div>
+
+			<img class="w-100 position-absolute start-0 bottom-0 z-top" src="<?php echo get_template_directory_uri(); ?>/assets/img/banner-mask.svg" alt="hero graphic">
+
+		</div>
 
     <?php get_template_part( 'template-parts/flexible-page-content' ); ?>
 
 <?php get_footer(); ?>
+
+
+<!-- Initialize Swiper -->
+<script>
+	var swiper = new Swiper('.hpSwiper', {
+	spaceBetween: 0,
+	effect: 'fade',
+	loop: true,
+	autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+	});
+</script>
--- template-parts/banner.php
+++ template-parts/banner.php
@@ -13,63 +13,6 @@ if ( empty( $thumb_alt ) ) {
 }
 ?>
 
-<?php if ( is_front_page()  ) : ?>
-	<div class="container-fluid position-relative d-flex align-items-center mb-4 mb-lg-2 bg-grad text-white hp-hero">
-			<div class="container pt-5 pt-lg-2 pb-4 py-lg-5 z-top-top">
-				<div class="row justify-content-center">
-					<div class="col-lg-8 text-center">
-						<h1 class="mt-0 mb-4">
-							<?php the_title(); ?>
-						</h1>
-						<?php if( get_field('hero_intro_text') ): ?>
-							<p class=""><?php the_field('hero_intro_text'); ?></p>
-						<?php endif; ?>
-
-						<div class="w-100 d-flex justify-content-center flex-wrap">
-							<?php 
-							$link = get_field('hero_button_link');
-							if( $link ): 
-								$link_url = $link['url'];
-								$link_title = $link['title'];
-								$link_target = $link['target'] ? $link['target'] : '_self';
-								?>
-								<a class="btn-white mx-2 mb-3" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-							<?php endif; ?>
-							<?php 
-							$link_secondary = get_field('hero_button_link_secondary');
-							if( $link_secondary ): 
-								$link_url = $link_secondary['url'];
-								$link_title = $link_secondary['title'];
-								$link_target = $link_secondary['target'] ? $link['target'] : '_self';
-								?>
-								<a class="btn-outline mx-2 mb-3" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-							<?php endif; ?>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<!-- Swiper -->
-			<div class="swiper hpSwiper position-absolute start-0 top-0">
-				<div class="swiper-wrapper">
-					<div class="swiper-slide">
-						<img src="https://clelandroofingsolutions.co.uk/wp-content/uploads/2024/05/Cleland-Roofing-1-Keydrone-Web.jpg" />
-					</div>
-					<div class="swiper-slide">
-						<img src="https://clelandroofingsolutions.co.uk/wp-content/uploads/2024/06/company_121783_2ae46e00-0ece-11ef-9f9a-f1477d54dbd8.jpg" />
-					</div>
-					<div class="swiper-slide">
-						<img src="https://clelandroofingsolutions.co.uk/wp-content/uploads/2024/06/company_121783_82f88c70-f8da-11ee-83fc-4b3478b91aa4.jpg" />
-					</div>
-				</div>
-			</div>
-			<div class="position-absolute start-0 left-0 w-100 h-100 z-top bg-grad opacity-75"></div>
-
-			<img class="w-100 position-absolute start-0 bottom-0 z-top" src="<?php echo get_template_directory_uri(); ?>/assets/img/banner-mask.svg" alt="hero graphic">
-
-		</div>
-
-<?php else: ?> 
 
 	<div class="container-fluid position-relative d-flex align-items-center bg-grad 
 			<?php if (  is_page( 'our-work' )) : ?>
@@ -126,19 +69,3 @@ if ( empty( $thumb_alt ) ) {
 			<?php endif; ?> 
 
 		</div>
-
-<?php endif; ?> 
-
-
-<!-- Initialize Swiper -->
-<script>
-	var swiper = new Swiper('.hpSwiper', {
-	spaceBetween: 0,
-	effect: 'fade',
-	loop: true,
-	autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-        },
-	});
-</script>
