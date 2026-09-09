28222f20fc5801174cb5faecb4dd59a5ece4eb96
alimont01 <hello@alimontgomery.com>
2026-09-09 14:55:08 +0100

    hp before after

--- template-parts/flexi-blocks/recent-roofing-block.php
+++ template-parts/flexi-blocks/recent-roofing-block.php
@@ -2,10 +2,12 @@
 	<div class="container text-white z-top">
 		<div class="row justify-content-center py-5">
 			<div class="col-12 col-lg-7 text-center">
-				<h2 class="mt-0 mb-3">
-					Recent Roofing Projects
-				</h2>
-				<p>Explore completed roofing projects across Scotland and Northern England, including domestic, commercial, repair, and specialist roofing work.</p>
+				<?php if( get_sub_field('block_title_recent') ): ?>
+					<h2 class="mt-0 mb-3"><?php the_sub_field('block_title_recent'); ?></h2>
+				<?php endif; ?>
+				<?php if( get_sub_field('intro_text_recent') ): ?>
+					<p class=""><?php the_sub_field('intro_text_recent'); ?></p>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
@@ -14,19 +16,25 @@
 		<div class="before-after" style="--position: 50%;">
 
 			<!-- Before image -->
-			<img
-				src="<?php echo get_template_directory_uri(); ?>/assets/img/roof-before.jpg"
-				alt="Roof before restoration"
+			<?php 
+			$image_before = get_sub_field('left_image_before');
+			if( !empty( $image_before ) ): ?>
+				<img src="<?php echo esc_url($image_before['url']); ?>"  
 				class="before-after__image before-after"
-			>
+				alt="<?php echo esc_attr($image_before['alt']); ?>" 
+			/>
+			<?php endif; ?>
 
 			<!-- After image -->
 			<div class="before-after__after">
-				<img
-					src="<?php echo get_template_directory_uri(); ?>/assets/img/roof-after.jpg"
-					alt="Roof after restoration"
+				<?php 
+				$image_after = get_sub_field('right_image_after');
+				if( !empty( $image_after ) ): ?>
+					<img src="<?php echo esc_url($image_after['url']); ?>" a 
 					class="before-after__image before-after"
-				>
+					lt="<?php echo esc_attr($image_after['alt']); ?>" 
+				/>
+				<?php endif; ?>
 			</div>
 
 			<!-- Labels -->
@@ -63,7 +71,15 @@
 	<div class="container z-top">
 		<div class="row">
 			<div class="col-12 text-center">
-				<a class="btn-white mt-2 d-inline-block" href="/our-work/">View our work</a>
+				<?php 
+				$link = get_sub_field('button_link_recent');
+				if( $link ): 
+					$link_url = $link['url'];
+					$link_title = $link['title'];
+					$link_target = $link['target'] ? $link['target'] : '_self';
+					?>
+					<a class="btn-white mt-2 d-inline-block" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
